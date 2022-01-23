826fddbd1170d35186f92766baa4aaa0e17e2792
Hala Zbib <halazbib22@gmail.com>
2022-01-23 18:55:44 +0200

    DONE!

--- game.js
+++ game.js
@@ -1,53 +1,56 @@
-var lost = false;
-var score = 0;
-
 window.onload = function () {
-  document.getElementById("start").addEventListener("click", started);
-  document.getElementById("end").addEventListener("mouseover", ended);
-  var boundaries = document.querySelectorAll("#game div.boundary");
-  for (var i = 0; i < boundaries.length; i++) {
-    boundaries[i].onmouseover = boundariesCrossed;
-  }
-
-  function boundariesCrossed() {
-    lost = true;
-    score -= 10;
-    console.log(score);
-    document.getElementById("status").innerHTML =
-      "hehe, you lost :'( | Score = " + score;
-    var boundaries = document.querySelectorAll("#game div.boundary");
-    for (var i = 0; i < boundaries.length; i++) {
-      boundaries[i].classList.add("youlose");
-    }
-  }
+  var lost = false;
+  var score = 0;
+  var Status = document.getElementById("status");
+  var S = document.getElementById("start");
+  var E = document.getElementById("end");
+  var boundaries = document.getElementsByClassName("boundary");
+  var box = document.getElementById("game");
+  S.addEventListener("click", started);
+  boundaries[5].style.textAlign = "center";
+  boundaries[5].innerText = score;
 
   function started() {
     lost = false;
-    document.getElementById("status").innerHTML =
-      'Begin by moving your mouse over the "S".';
-    var boundaries = document.querySelectorAll("#game div.boundary");
+    E.addEventListener("mouseover", ended);
+    for (var i = 0; i < boundaries.length; i++) {
+      boundaries[i].addEventListener("mouseover", boundariesCrossed);
+      boundaries[i].style.backgroundColor = null;
+    }
+    Status.innerHTML = "Game in progress... ";
+    box.addEventListener("mouseleave", boundariesCrossed);
     for (var i = 0; i < boundaries.length; i++) {
       boundaries[i].classList.remove("youlose");
     }
   }
 
+  function boundariesCrossed() {
+    lost = true;
+    score -= 10;
+    box.removeEventListener("mouseleave", boundariesCrossed);
+    Status.innerHTML = "hehe, you lost :'( | Press S to start over";
+    boundaries[5].innerText = score;
+    for (var i = 0; i < boundaries.length; i++) {
+      boundaries[i].removeEventListener("mouseover", boundariesCrossed);
+      boundaries[i].classList.add("youlose");
+    }
+  }
+
   function ended() {
+    box.removeEventListener("mouseleave", boundariesCrossed);
+    E.removeEventListener("mouseover", ended);
     if (lost == false) {
       score += 5;
-      console.log(score);
-      document.getElementById("status").innerHTML =
-        "You won :/ | Score= " + score;
-      var boundaries = document.querySelectorAll("#game div.boundary");
+      Status.innerHTML = "You won :/ | Press S to start over";
+      boundaries[5].innerText = score;
       for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].removeEventListener("mouseover", boundariesCrossed);
         boundaries[i].style.backgroundColor = "#88ff88";
       }
-    }
-    else{
-        var boundaries = document.querySelectorAll("#game div.boundary");
+    } else {
       for (var i = 0; i < boundaries.length; i++) {
         boundaries[i].style.backgroundColor = "#8888ff";
+      }
     }
-
   }
-}
-}
+};
